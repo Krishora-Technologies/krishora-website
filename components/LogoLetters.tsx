1c8ef66bc37982cf8dd3a34aa5ef1b559a940767
omthakur <omthakur2366@gmail.com>
2026-04-22 18:42:14 +0530

    fix: refine R letter shape and balance character spacing

--- components/LogoLetters.tsx
+++ components/LogoLetters.tsx
@@ -48,23 +48,23 @@ export default function LogoLetters() {
         </g>
 
         {/* R */}
-        <g transform="translate(90,0)" className="kr-letter-wrapper">
+        <g transform="translate(95,0)" className="kr-letter-wrapper">
           <g className="kr-letter-ghost" stroke="rgba(0,0,0,0.1)">
             <g clipPath="url(#c-r1)">
               <line x1="15" y1="10" x2="15" y2="90" />
-              <path d="M 35 15 L 55 15 A 17 17 0 0 1 72 32 A 17 17 0 0 1 55 50 L 35 50 M 45 50 L 75 105" />
+              <path d="M 15 15 L 55 15 A 17 17 0 0 1 72 32 A 17 17 0 0 1 55 50 L 30 50 M 45 50 L 75 105" />
             </g>
           </g>
           <g className="kr-letter-solid" stroke="var(--text-primary, #000)" style={{ clipPath: "var(--clipPath, inset(100% 0 0 0))" }}>
             <g clipPath="url(#c-r1)">
               <line x1="15" y1="10" x2="15" y2="90" />
-              <path d="M 35 15 L 55 15 A 17 17 0 0 1 72 32 A 17 17 0 0 1 55 50 L 35 50 M 45 50 L 75 105" />
+              <path d="M 15 15 L 55 15 A 17 17 0 0 1 72 32 A 17 17 0 0 1 55 50 L 30 50 M 45 50 L 75 105" />
             </g>
           </g>
         </g>
 
         {/* I */}
-        <g transform="translate(180,0)" className="kr-letter-wrapper">
+        <g transform="translate(185,0)" className="kr-letter-wrapper">
           <g className="kr-letter-ghost" stroke="rgba(0,0,0,0.1)">
             <g clipPath="url(#c-i)">
               <line x1="15" y1="10" x2="15" y2="90" />
@@ -78,7 +78,7 @@ export default function LogoLetters() {
         </g>
 
         {/* S */}
-        <g transform="translate(220,0)" className="kr-letter-wrapper">
+        <g transform="translate(215,0)" className="kr-letter-wrapper">
           <g className="kr-letter-ghost" stroke="rgba(0,0,0,0.1)">
             <g clipPath="url(#c-s)">
               <path d="M 75 15 L 45 15 A 17 17 0 0 0 28 32 A 17 17 0 0 0 45 50 L 55 50 A 17 17 0 0 1 72 67 A 17 17 0 0 1 55 85 L 15 85" />
@@ -92,7 +92,7 @@ export default function LogoLetters() {
         </g>
 
         {/* H */}
-        <g transform="translate(310,0)" className="kr-letter-wrapper">
+        <g transform="translate(305,0)" className="kr-letter-wrapper">
           <g className="kr-letter-ghost" stroke="rgba(0,0,0,0.1)">
             <g clipPath="url(#c-h)">
               <line x1="15" y1="10" x2="15" y2="90" />
@@ -110,7 +110,7 @@ export default function LogoLetters() {
         </g>
 
         {/* O */}
-        <g transform="translate(400,0)" className="kr-letter-wrapper">
+        <g transform="translate(395,0)" className="kr-letter-wrapper">
           <g className="kr-letter-ghost" stroke="rgba(0,0,0,0.1)">
             <g clipPath="url(#c-o)">
               <rect x="15" y="15" width="60" height="70" rx="25" />
@@ -124,23 +124,23 @@ export default function LogoLetters() {
         </g>
 
         {/* R */}
-        <g transform="translate(500,0)" className="kr-letter-wrapper">
+        <g transform="translate(485,0)" className="kr-letter-wrapper">
           <g className="kr-letter-ghost" stroke="rgba(0,0,0,0.1)">
             <g clipPath="url(#c-r2)">
               <line x1="15" y1="10" x2="15" y2="90" />
-              <path d="M 35 15 L 55 15 A 17 17 0 0 1 72 32 A 17 17 0 0 1 55 50 L 35 50 M 45 50 L 75 105" />
+              <path d="M 15 15 L 55 15 A 17 17 0 0 1 72 32 A 17 17 0 0 1 55 50 L 30 50 M 45 50 L 75 105" />
             </g>
           </g>
           <g className="kr-letter-solid" stroke="var(--text-primary, #000)" style={{ clipPath: "var(--clipPath, inset(100% 0 0 0))" }}>
             <g clipPath="url(#c-r2)">
               <line x1="15" y1="10" x2="15" y2="90" />
-              <path d="M 35 15 L 55 15 A 17 17 0 0 1 72 32 A 17 17 0 0 1 55 50 L 35 50 M 45 50 L 75 105" />
+              <path d="M 15 15 L 55 15 A 17 17 0 0 1 72 32 A 17 17 0 0 1 55 50 L 30 50 M 45 50 L 75 105" />
             </g>
           </g>
         </g>
 
         {/* Λ */}
-        <g transform="translate(590,0)" className="kr-letter-wrapper">
+        <g transform="translate(580,0)" className="kr-letter-wrapper">
           <g className="kr-letter-ghost" stroke="rgba(0,0,0,0.1)">
             <g clipPath="url(#c-a)">
               <path d="M 10 105 L 45 -5 L 80 105" />
